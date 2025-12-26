34ad1c49f488c3a06ce707cbc08386cf15c07bfc
Saad Abbasi <07saadabbasi@gmail.com>
2025-12-26 10:54:37 +0500

    Add CORS middleware to FastAPI

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = FastAPI()
+
+# Allow any website to call your API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # <- this allows all domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load saved model and tokenizer
 model = load_model("text_model.h5")
@@ -25,5 +35,6 @@ def predict(text: str, next_words: int = 3):
         token_list = tokenizer.texts_to_sequences([seed_text])[0]
         token_list = pad_sequences([token_list[-max_len:]], maxlen=max_len)
         predicted_index = np.argmax(model.predict(token_list, verbose=0))
-        seed_text += " " + tokenizer.index_word.get(predicted_index, "")
+        output_word = tokenizer.index_word.get(predicted_index, "")
+        seed_text += " " + output_word
     return {"prediction": seed_text}
